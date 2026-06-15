eea64445fd98b7c12d210deb29f54610d208eeb1
AbhishekSinghYadav15 <abhishek7898yadav@gmail.com>
2026-06-15 16:50:04 +0530

    summer assignment

---
+++ Day-12/programm4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int isPerfect(int n) {
+    int sum = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0)
+            sum += i;
+    }
+    return sum == n;
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (isPerfect(n))
+        printf("%d is a Perfect Number\n", n);
+    else
+        printf("%d is Not a Perfect Number\n", n);
+    return 0;
+}

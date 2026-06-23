12d53bd2bb5abcf6b51e0e622cad82b2101777eb
AbhishekSinghYadav15 <abhishek7898yadav@gmail.com>
2026-06-23 22:04:14 +0530

    summer assignment

---
+++ Day-21/program1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int length = 0;
+    while (str[length] != '\0')
+        length++;
+
+    printf("Length of string = %d\n", length);
+
+    return 0;
+}
